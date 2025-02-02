b10bf7e461c84b19cafe3bc58f82f3d4c8b39d64
Stefan Urbanek <stefan@agentfarms.net>
2025-02-02 18:50:24 +0100

    Rewrote expression AST
    Expression AST is now just plain enum, not a collection of classes. It
    was an obsolete design, where we were expecting to have a more complex
    language. It is no longer necessasry, as a language is out of scope of
    this project. Expressions are enough.

--- Sources/PoieticCore/ExpressionParser/ExpressionParser.swift
+++ Sources/PoieticCore/ExpressionParser/ExpressionParser.swift
@@ -108,12 +108,12 @@ public class ExpressionParser {
 
     /// Parse an integer or a float.
     ///
-    func number() -> (any ExpressionSyntax)? {
+    func number() -> ExpressionAST? {
         if let token = accept(.int) {
-            return LiteralSyntax(type: .int, literal: token)
+            return .intLiteral(token)
         }
         else if let token = accept(.double) {
-            return LiteralSyntax(type: .double, literal: token)
+            return .doubleLiteral(token)
         }
         else {
             return nil
@@ -124,45 +124,41 @@ public class ExpressionParser {
     ///
     ///     variable_call -> IDENTIFIER ["(" ARGUMENTS ")"]
     ///
-    func variable_or_call() throws (ExpressionSyntaxError) -> (any ExpressionSyntax)? {
+    func variable_or_call() throws (ExpressionSyntaxError) -> ExpressionAST? {
         guard let ident = identifier() else {
             return nil
         }
 
         if let lparen = accept(.leftParen) {
-            var arguments: [FunctionArgumentSyntax] = []
+            var arguments: [ExpressionAST] = []
             repeat {
                 guard let expr = try expression() else {
                     break
                 }
                 
                 if let comma = accept(.comma) {
-                    let arg = FunctionArgumentSyntax(argument: expr,
-                                                     trailingComma: comma)
+                    let arg: ExpressionAST = .functionArgument(argument:expr, comma: comma)
                     arguments.append(arg)
                 }
                 else {
-                    let arg = FunctionArgumentSyntax(argument: expr,
-                                                     trailingComma: nil)
+                    let arg: ExpressionAST = .functionArgument(argument:expr, comma: nil)
                     arguments.append(arg)
                     break
                 }
             } while true
 
-            let argList = FunctionArgumentListSyntax(arguments: arguments)
-            
             guard let rparen = accept(.rightParen) else {
-                throw ExpressionSyntaxError.missingRightParenthesis
+                throw .missingRightParenthesis
             }
 
-            return FunctionCallSyntax(name: ident,
-                                      leftParen: lparen,
-                                      arguments: argList,
-                                      rightParen: rparen)
+            return .functionCall(name: ident,
+                                 arguments: arguments,
+                                 openParen: lparen,
+                                 closeParen: rparen)
         }
         else {
             // We got a variable
-            return VariableSyntax(variable: ident)
+            return .variable(ident)
         }
     }
     
@@ -170,7 +166,7 @@ public class ExpressionParser {
     ///
     ///     primary -> NUMBER | STRING | VARIABLE_OR_CALL | "(" expression ")" ;
     ///
-    func primary() throws (ExpressionSyntaxError) -> (any ExpressionSyntax)? {
+    func primary() throws (ExpressionSyntaxError) -> ExpressionAST? {
         // TODO: true, false, nil
         if let node = number() {
             return node
@@ -182,11 +178,11 @@ public class ExpressionParser {
         else if let lparen = accept(.leftParen) {
             if let expr = try expression() {
                 guard let rparen = accept(.rightParen) else {
-                    throw ExpressionSyntaxError.missingRightParenthesis
+                    throw .missingRightParenthesis
                 }
-                return ParenthesisSyntax(leftParen: lparen,
-                                         expression: expr,
-                                         rightParen: rparen)
+                return .parenthesis(expression: expr,
+                                    openParen: lparen,
+                                    closeParen: rparen)
             }
         }
         return nil
@@ -196,14 +192,13 @@ public class ExpressionParser {
     ///
     ///     unary -> "-" unary | primary ;
     ///
-    func unary() throws (ExpressionSyntaxError) -> (any ExpressionSyntax)? {
+    func unary() throws (ExpressionSyntaxError) -> ExpressionAST? {
         // TODO: Add '!'
         if let op = `operator`("-") {
-            guard let right = try unary() else {
-                throw ExpressionSyntaxError.expressionExpected
+            guard let operand = try unary() else {
+                throw .expressionExpected
             }
-            return UnaryOperatorSyntax(op: op,
-                                       operand: right)
+            return .unaryOperator(operator: op, operand: operand)
         }
         else {
             return try primary()
@@ -215,18 +210,16 @@ public class ExpressionParser {
     ///
     ///     factor -> unary ( ( "/" | "*" ) unary )* ;
     ///
-    func factor() throws (ExpressionSyntaxError) -> (any ExpressionSyntax)? {
-        guard var left: any ExpressionSyntax = try unary() else {
+    func factor() throws (ExpressionSyntaxError) -> ExpressionAST? {
+        guard var left = try unary() else {
             return nil
         }
         
-        while let op = `operator`("*") ?? `operator`("/") ?? `operator`("%"){
+        while let op = `operator`("*") ?? `operator`("/") ?? `operator`("%") {
             guard let right = try unary() else {
-                throw ExpressionSyntaxError.expressionExpected
+                throw .expressionExpected
             }
-            left = BinaryOperatorSyntax(leftOperand: left,
-                                         op: op,
-                                         rightOperand: right)
+            left = .binaryOperator(operator: op, left: left, right: right)
         }
         
         return left
@@ -236,18 +229,16 @@ public class ExpressionParser {
     ///
     ///     term -> factor ( ( "-" | "+" ) factor )* ;
     ///
-    func term() throws (ExpressionSyntaxError) -> (any ExpressionSyntax)? {
-        guard var left: any ExpressionSyntax = try factor() else {
+    func term() throws (ExpressionSyntaxError) -> ExpressionAST? {
+        guard var left = try factor() else {
             return nil
         }
         
         while let op = `operator`("+") ?? `operator`("-") {
             guard let right = try factor() else {
-                throw ExpressionSyntaxError.expressionExpected
+                throw .expressionExpected
             }
-            left = BinaryOperatorSyntax(leftOperand: left,
-                                         op: op,
-                                         rightOperand: right)
+            left = .binaryOperator(operator: op, left: left, right: right)
         }
         
         return left
@@ -257,8 +248,8 @@ public class ExpressionParser {
     ///
     ///     term -> factor ( ( "-" | "+" ) factor )* ;
     ///
-    func comparison_expression() throws (ExpressionSyntaxError) -> (any ExpressionSyntax)? {
-        guard var left: any ExpressionSyntax = try term() else {
+    func comparison_expression() throws (ExpressionSyntaxError) -> ExpressionAST? {
+        guard var left = try term() else {
             return nil
         }
         
@@ -267,11 +258,9 @@ public class ExpressionParser {
                 ?? `operator`(">")
                 ?? `operator`(">="){
             guard let right = try term() else {
-                throw ExpressionSyntaxError.expressionExpected
+                throw .expressionExpected
             }
-            left = BinaryOperatorSyntax(leftOperand: left,
-                                         op: op,
-                                         rightOperand: right)
+            left = .binaryOperator(operator: op, left: left, right: right)
         }
         
         return left
@@ -281,24 +270,22 @@ public class ExpressionParser {
     ///
     ///     term -> factor ( ( "-" | "+" ) factor )* ;
     ///
-    func equality_expression() throws (ExpressionSyntaxError) -> (any ExpressionSyntax)? {
-        guard var left: any ExpressionSyntax = try comparison_expression() else {
+    func equality_expression() throws (ExpressionSyntaxError) -> ExpressionAST? {
+        guard var left = try comparison_expression() else {
             return nil
         }
         
         while let op = `operator`("==") ?? `operator`("!=") {
             guard let right = try comparison_expression() else {
-                throw ExpressionSyntaxError.expressionExpected
+                throw .expressionExpected
             }
-            left = BinaryOperatorSyntax(leftOperand: left,
-                                         op: op,
-                                         rightOperand: right)
+            left = .binaryOperator(operator: op, left: left, right: right)
         }
         
         return left
     }
 
-    func expression() throws (ExpressionSyntaxError) -> ExpressionSyntax? {
+    func expression() throws (ExpressionSyntaxError) -> ExpressionAST? {
         return try equality_expression()
     }
     
@@ -308,305 +295,11 @@ public class ExpressionParser {
     /// - Throws: `SyntaxError` when there is an issue with the expression.
     public func parse() throws (ExpressionSyntaxError) -> UnboundExpression {
         guard let expr = try expression() else {
-            throw ExpressionSyntaxError.expressionExpected
+            throw .expressionExpected
         }
         
         if currentToken?.type != .empty {
-            throw ExpressionSyntaxError.unexpectedToken
-        }
-        return expr.toExpression()
-    }
-    
-}
-
-
-public class ExpressionParser2 {
-    var lexer: ExpressionLexer
-    var currentToken: ExpressionToken?
-    
-    // TODO: Add: public static parsing(string:) throws -> UnboundExpression
-    /// Creates a new parser using an expression lexer.
-    ///
-    public init(lexer: ExpressionLexer) {
-        self.lexer = lexer
-        advance()
-    }
-    
-    /// Creates a new parser for an expression source string.
-    ///
-    public convenience init(string: String) {
-        self.init(lexer: ExpressionLexer(string: string))
-    }
-    
-    /// True if the parser is at the end of the source.
-    var atEnd: Bool {
-        if let token = currentToken {
-            return token.type == .empty
-        }
-        else {
-            return true
-        }
-    }
-    
-    /// Advance to the next token.
-    ///
-    func advance() {
-        currentToken = lexer.next()
-    }
-    
-    /// Accent a token a type ``type``.
-    ///
-    /// - Returns: A token if the token matches the expected type, ``nil`` if
-    ///     the token does not match the expected type.
-    ///
-    func accept(_ type: ExpressionToken.TokenType) -> ExpressionToken? {
-        guard let token = currentToken else {
-            return nil
-        }
-        if token.type == type {
-            advance()
-            return token
-        }
-        else {
-            return nil
-        }
-    }
-
-    // ----------------------------------------------------------------
-    
-    /// Parse an operator.
-    func `operator`(_ op: String) -> ExpressionToken? {
-        guard let token = currentToken else {
-            return nil
-        }
-        if token.type == .operator && token.text == op {
-            advance()
-            return token
-        }
-        else {
-            return nil
-        }
-
-    }
-    
-    /// Parse an identifier - a variable name or a function name.
-    ///
-    func identifier() -> ExpressionToken? {
-        if let token = accept(.identifier) {
-            return token
-        }
-        else {
-            return nil
-        }
-    }
-
-    /// Parse an integer or a float.
-    ///
-    func number() -> (any ExpressionSyntax)? {
-        if let token = accept(.int) {
-            return LiteralSyntax(type: .int, literal: token)
-        }
-        else if let token = accept(.double) {
-            return LiteralSyntax(type: .double, literal: token)
-        }
-        else {
-            return nil
-        }
-    }
-    
-    /// Rule:
-    ///
-    ///     variable_call -> IDENTIFIER ["(" ARGUMENTS ")"]
-    ///
-    func variable_or_call() throws (ExpressionSyntaxError) -> (any ExpressionSyntax)? {
-        guard let ident = identifier() else {
-            return nil
-        }
-
-        if let lparen = accept(.leftParen) {
-            var arguments: [FunctionArgumentSyntax] = []
-            repeat {
-                guard let expr = try expression() else {
-                    break
-                }
-                
-                if let comma = accept(.comma) {
-                    let arg = FunctionArgumentSyntax(argument: expr,
-                                                     trailingComma: comma)
-                    arguments.append(arg)
-                }
-                else {
-                    let arg = FunctionArgumentSyntax(argument: expr,
-                                                     trailingComma: nil)
-                    arguments.append(arg)
-                    break
-                }
-            } while true
-
-            let argList = FunctionArgumentListSyntax(arguments: arguments)
-            
-            guard let rparen = accept(.rightParen) else {
-                throw ExpressionSyntaxError.missingRightParenthesis
-            }
-
-            return FunctionCallSyntax(name: ident,
-                                      leftParen: lparen,
-                                      arguments: argList,
-                                      rightParen: rparen)
-        }
-        else {
-            // We got a variable
-            return VariableSyntax(variable: ident)
-        }
-    }
-    
-    /// Rule:
-    ///
-    ///     primary -> NUMBER | STRING | VARIABLE_OR_CALL | "(" expression ")" ;
-    ///
-    func primary() throws (ExpressionSyntaxError) -> (any ExpressionSyntax)? {
-        // TODO: true, false, nil
-        if let node = number() {
-            return node
-        }
-        else if let node = try variable_or_call() {
-            return node
-        }
-
-        else if let lparen = accept(.leftParen) {
-            if let expr = try expression() {
-                guard let rparen = accept(.rightParen) else {
-                    throw ExpressionSyntaxError.missingRightParenthesis
-                }
-                return ParenthesisSyntax(leftParen: lparen,
-                                         expression: expr,
-                                         rightParen: rparen)
-            }
-        }
-        return nil
-    }
-    
-    /// Rule:
-    ///
-    ///     unary -> "-" unary | primary ;
-    ///
-    func unary() throws (ExpressionSyntaxError) -> (any ExpressionSyntax)? {
-        // TODO: Add '!'
-        if let op = `operator`("-") {
-            guard let right = try unary() else {
-                throw ExpressionSyntaxError.expressionExpected
-            }
-            return UnaryOperatorSyntax(op: op,
-                                       operand: right)
-        }
-        else {
-            return try primary()
-        }
-        
-    }
-
-    /// Rule:
-    ///
-    ///     factor -> unary ( ( "/" | "*" ) unary )* ;
-    ///
-    func factor() throws (ExpressionSyntaxError) -> (any ExpressionSyntax)? {
-        guard var left: any ExpressionSyntax = try unary() else {
-            return nil
-        }
-        
-        while let op = `operator`("*") ?? `operator`("/") ?? `operator`("%"){
-            guard let right = try unary() else {
-                throw ExpressionSyntaxError.expressionExpected
-            }
-            left = BinaryOperatorSyntax(leftOperand: left,
-                                         op: op,
-                                         rightOperand: right)
-        }
-        
-        return left
-    }
-
-    /// Rule:
-    ///
-    ///     term -> factor ( ( "-" | "+" ) factor )* ;
-    ///
-    func term() throws (ExpressionSyntaxError) -> (any ExpressionSyntax)? {
-        guard var left: any ExpressionSyntax = try factor() else {
-            return nil
-        }
-        
-        while let op = `operator`("+") ?? `operator`("-") {
-            guard let right = try factor() else {
-                throw ExpressionSyntaxError.expressionExpected
-            }
-            left = BinaryOperatorSyntax(leftOperand: left,
-                                         op: op,
-                                         rightOperand: right)
-        }
-        
-        return left
-    }
-    
-    /// Rule:
-    ///
-    ///     term -> factor ( ( "-" | "+" ) factor )* ;
-    ///
-    func comparison_expression() throws (ExpressionSyntaxError) -> (any ExpressionSyntax)? {
-        guard var left: any ExpressionSyntax = try term() else {
-            return nil
-        }
-        
-        while let op = `operator`("<")
-                ?? `operator`("<=")
-                ?? `operator`(">")
-                ?? `operator`(">="){
-            guard let right = try term() else {
-                throw ExpressionSyntaxError.expressionExpected
-            }
-            left = BinaryOperatorSyntax(leftOperand: left,
-                                         op: op,
-                                         rightOperand: right)
-        }
-        
-        return left
-    }
-
-    /// Rule:
-    ///
-    ///     term -> factor ( ( "-" | "+" ) factor )* ;
-    ///
-    func equality_expression() throws (ExpressionSyntaxError) -> (any ExpressionSyntax)? {
-        guard var left: any ExpressionSyntax = try comparison_expression() else {
-            return nil
-        }
-        
-        while let op = `operator`("==") ?? `operator`("!=") {
-            guard let right = try comparison_expression() else {
-                throw ExpressionSyntaxError.expressionExpected
-            }
-            left = BinaryOperatorSyntax(leftOperand: left,
-                                         op: op,
-                                         rightOperand: right)
-        }
-        
-        return left
-    }
-
-    func expression() throws (ExpressionSyntaxError) -> ExpressionSyntax? {
-        return try equality_expression()
-    }
-    
-    
-    /// Parse the expression and return an unbound arithmetic expression.
-    ///
-    /// - Throws: `SyntaxError` when there is an issue with the expression.
-    public func parse() throws (ExpressionSyntaxError) -> UnboundExpression {
-        guard let expr = try expression() else {
-            throw ExpressionSyntaxError.expressionExpected
-        }
-        
-        if currentToken?.type != .empty {
-            throw ExpressionSyntaxError.unexpectedToken
+            throw .unexpectedToken
         }
         return expr.toExpression()
     }
