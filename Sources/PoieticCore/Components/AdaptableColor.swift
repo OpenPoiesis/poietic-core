8075d358f1146bd0cadd5ae37f99c541fd1b9feb
Stefan Urbanek <stefan@agentfarms.net>
2026-03-11 22:24:02 +0100

    Added AdaptableColorKey

---
+++ Sources/PoieticCore/Components/AdaptableColor.swift
@@ -0,0 +1,22 @@
+//
+//  AdaptableColor.swift
+//  poietic-core
+//
+//  Created by Stefan Urbanek on 09/12/2025.
+//
+
+
+public enum AdaptableColorKey: String, CaseIterable, Hashable, Sendable {
+    case purple = "purple"
+    case red = "red"
+    case pink = "pink"
+    case brown = "brown"
+    case orange = "orange"
+    case yellow = "yellow"
+    case lime = "lime"
+    case green = "green"
+    case cyan = "cyan"
+    case teal = "teal"
+    case blue = "blue"
+    case indigo = "indigo"
+}
