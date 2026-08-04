1032de2bc60a226766e33b0f31cdb14536c55a8a
Stefan Urbanek <stefan@agentfarms.net>
2026-08-04 12:26:10 +0200

    Added visual metadata system

---
+++ Sources/PoieticCore/System/VisualMetadataSystem.swift
@@ -0,0 +1,25 @@
+//
+//  VisualMetadataSystem.swift
+//  poietic-core
+//
+//  Created by Stefan Urbanek on 04/08/2026.
+//
+
+
+/// System that prepares basic visual metadata.
+///
+/// - **Input:** Design objects from current frame with trait ``Trait/NumericValue``.
+/// - **Output:** Add ``DisplayValueBounds`` component to the design object entity.
+/// - **Forgiveness:** Nothing to be forgiven.
+///
+public struct VisualMetadataSystem: System {
+    public init(_ world: World) { }
+    public func update(_ world: World) throws(InternalSystemError) {
+        guard let frame = world.frame else { return }
+        
+        for object in frame.filter(trait: .NumericValue) {
+            guard let entity = world.entity(object.objectID) else { continue }
+            entity.setComponent(DisplayValueBounds(from: object))
+        }
+    }
+}
